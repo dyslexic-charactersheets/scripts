1963c1fb4c31484c5e012dcf6425e84116d63605
Marcus Downing <marcus@bang-on.net>
2017-03-26 14:13:04 +0100

    Translation updates

--- Export all as PDF.jsx
+++ Export all as PDF.jsx
@@ -2,10 +2,8 @@
 
 #include i18n_tools.jsxinc
 
-// var sourceFolder = new Folder( '/Users/Marcus Downing/Documents/GitHub/charactersheets/Pathfinder/Archetypes/Monk' );
-// var destinationFolder = new Folder( '/Users/Marcus Downing/Documents/GitHub/charactersheets/Composer/public/pdf/pathfinder/Archetypes/Monk' );
-var sourceFolder = baseFolder;
-var destinationFolder = baseFolder+'../Character Sheets PDF/'
+var sourceFolder = pagesFolder;
+var destinationFolder = assetsFolder;
 
 i18n.init();
 
@@ -14,64 +12,55 @@ exportFolderAsPDF(new Folder(sourceFolder+'3.5'), new Folder(destinationFolder+'
 exportFolderAsPDF(new Folder(sourceFolder+'All'), new Folder(destinationFolder+'pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'All'), new Folder(destinationFolder+'dnd35'));
 
-alert("English done...");
-
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Italian/Pathfinder'), new Folder(destinationFolder+'languages/italian/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Italian/3.5'), new Folder(destinationFolder+'languages/italian/dnd35'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Italian/All'), new Folder(destinationFolder+'languages/italian/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Italian/All'), new Folder(destinationFolder+'languages/italian/dnd35'));
-
-alert("Italian done...");
 
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Spanish/Pathfinder'), new Folder(destinationFolder+'languages/spanish/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Spanish/3.5'), new Folder(destinationFolder+'languages/spanish/dnd35'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Spanish/All'), new Folder(destinationFolder+'languages/spanish/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Spanish/All'), new Folder(destinationFolder+'languages/spanish/dnd35'));
 
-alert("Spanish done...");
-
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Polish/Pathfinder'), new Folder(destinationFolder+'languages/polish/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Polish/3.5'), new Folder(destinationFolder+'languages/polish/dnd35'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Polish/All'), new Folder(destinationFolder+'languages/polish/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Polish/All'), new Folder(destinationFolder+'languages/polish/dnd35'));
 
-alert("Polish done...");
-
-/*
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Portuguese/Pathfinder'), new Folder(destinationFolder+'languages/portuguese/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Portuguese/3.5'), new Folder(destinationFolder+'languages/portuguese/dnd35'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Portuguese/All'), new Folder(destinationFolder+'languages/portuguese/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Portuguese/All'), new Folder(destinationFolder+'languages/portuguese/dnd35'));
-
-alert("Portuguese done...");
-*/
 
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/French/Pathfinder'), new Folder(destinationFolder+'languages/french/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/French/3.5'), new Folder(destinationFolder+'languages/french/dnd35'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/French/All'), new Folder(destinationFolder+'languages/french/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/French/All'), new Folder(destinationFolder+'languages/french/dnd35'));
 
-alert("French done...");
-
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/German/Pathfinder'), new Folder(destinationFolder+'languages/german/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/German/3.5'), new Folder(destinationFolder+'languages/german/dnd35'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/German/All'), new Folder(destinationFolder+'languages/german/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/German/All'), new Folder(destinationFolder+'languages/german/dnd35'));
 
-alert("German done...");
-
-/*
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Russian/Pathfinder'), new Folder(destinationFolder+'languages/russian/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Russian/3.5'), new Folder(destinationFolder+'languages/russian/dnd35'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Russian/All'), new Folder(destinationFolder+'languages/russian/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/Russian/All'), new Folder(destinationFolder+'languages/russian/dnd35'));
 
-alert("Russian done...");
+exportFolderAsPDF(new Folder(sourceFolder+'Languages/Norwegian/Pathfinder'), new Folder(destinationFolder+'languages/norwegian/pathfinder'));
+exportFolderAsPDF(new Folder(sourceFolder+'Languages/Norwegian/3.5'), new Folder(destinationFolder+'languages/norwegian/dnd35'));
+exportFolderAsPDF(new Folder(sourceFolder+'Languages/Norwegian/All'), new Folder(destinationFolder+'languages/norwegian/pathfinder'));
+exportFolderAsPDF(new Folder(sourceFolder+'Languages/Norwegian/All'), new Folder(destinationFolder+'languages/norwegian/dnd35'));
+
+exportFolderAsPDF(new Folder(sourceFolder+'Languages/Dutch/Pathfinder'), new Folder(destinationFolder+'languages/dutch/pathfinder'));
+exportFolderAsPDF(new Folder(sourceFolder+'Languages/Dutch/3.5'), new Folder(destinationFolder+'languages/dutch/dnd35'));
+exportFolderAsPDF(new Folder(sourceFolder+'Languages/Dutch/All'), new Folder(destinationFolder+'languages/dutch/pathfinder'));
+exportFolderAsPDF(new Folder(sourceFolder+'Languages/Dutch/All'), new Folder(destinationFolder+'languages/dutch/dnd35'));
 
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/US English/Pathfinder'), new Folder(destinationFolder+'languages/american/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/US English/3.5'), new Folder(destinationFolder+'languages/american/dnd35'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/US English/All'), new Folder(destinationFolder+'languages/american/pathfinder'));
 exportFolderAsPDF(new Folder(sourceFolder+'Languages/US English/All'), new Folder(destinationFolder+'languages/american/dnd35'));
 
-alert("US English done...");
-*/
+alert("Done");
+
--- i18n apply all translations.jsx
+++ i18n apply all translations.jsx
@@ -8,22 +8,25 @@
 i18n.init();
 
 var folders = [ "Pathfinder", "3.5", "All", "Extra" ];
-var languages = [ /*"Italian", "Spanish", "Polish",*/ "German", "French" /*, "Portuguese", "Russian", "US English" */ ];
+var languages = [ "Italian", "Spanish", "Polish", "German", "French", "Portuguese", "Russian", "Dutch", "Norwegian", "US English" ];
 log("i18n: Preparing to translate into", languages);
 
 for (var i = 0; i < languages.length; i++) {
 	var language = languages[i];
 
 	i18n.language = i18n.esLanguages[language];
-	i18n.loadTranslations(new File(baseFolder + 'Languages/' + language + '.csv'));
+	i18n.loadTranslations(new File(pagesFolder + 'Languages/' + language + '.csv'));
 
 	for (var j = 0; j < folders.length; j++) {
 		var folder = folders[j];
-		var srcFolder = new Folder(baseFolder + folder);
-		var dstFolder = new Folder(baseFolder + 'Languages/' + language + '/' + folder);
+		var srcFolder = new Folder(pagesFolder + folder);
+		var dstFolder = new Folder(pagesFolder + 'Languages/' + language + '/' + folder);
 		i18n.applyTranslationsFolder(srcFolder, dstFolder);
+		i18n.tick();
 	}
-	alert(language+' finished');
+	log("i18: "+language+" finished");
+	i18n.tick();
+	//alert(language+' finished');
 }
 
 log("i18n: Translated "+i18n.countTranslatedLines+" strings from "+i18n.countTranslatedFiles+" files");
